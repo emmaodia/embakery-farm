092fdd5adff3a1a33fbd7596193261d7305278cb
emmaodia <odia.emma@gmail.com>
2022-07-13 23:32:19 +0100

    UG Token Contract added.

---
+++ contracts/UGToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract UGToken is ERC20, ERC20Burnable, Ownable {
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor() ERC20("UGToken", "UGT") {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
